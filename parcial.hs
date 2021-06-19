cf752c01c598f76767840f470624e22dd4b0cf00
Henry Gabriel <80067671+Henry-dotexe@users.noreply.github.com>
2021-06-19 15:12:33 -0300

    Update parcial.hs

--- parcial.hs
+++ parcial.hs
@@ -1,5 +1,34 @@
 {-
-Nombre: Apellido, Nombre
-Legajo: 999999-9
+Nombre: Soto, Henry
+Legajo: 177.160-7
 -}
+-- Punto 1, modelado de animal
+data Animal = Animal {
+    coef_intel :: Int,
+    especie :: String,
+    capacidades :: [String]
+}deriving(Show) 
 
+maximus = Animal {
+    coef_intel=10,
+    especie = "Perro",
+    capacidades = ["sentarse"]
+}
+
+dumbo = Animal {
+    coef_intel=25,
+    especie= "Elefante",
+    capacidades = ["volar"]
+}
+
+remy = Animal {
+    coef_intel=101,
+    especie = "Ratón",
+    capacidades = ["cocinar"]
+}
+type Transformacion = Animal ->Animal
+--Punto 2, transformar animales
+inteligenciaSuperior :: Int->Transformacion
+inteligenciaSuperior n animal = animal{
+    coef_intel= coef_intel animal + n
+} 
